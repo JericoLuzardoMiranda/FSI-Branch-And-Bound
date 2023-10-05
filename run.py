1efb7a8d79a7ba9f2c932b77b50dc70a67c5df34
JericoLuzardoMiranda <125827592+JericoLuzardoMiranda@users.noreply.github.com>
2023-10-05 11:06:33 +0100

    Añadido las pruebas

--- run.py
+++ run.py
@@ -2,12 +2,51 @@
 
 import search
 
-ab = search.GPSProblem('A', 'B'
-                       , search.romania)
+print("=== ID 1: Arad - Bucharest ===")
+ab = search.GPSProblem('A', 'B', search.romania)
 
+print("--- Amplitud ---")
 print(search.breadth_first_graph_search(ab).path())
+
+print("\n--- Profundidad ---")
 print(search.depth_first_graph_search(ab).path())
 
 # Result:
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
 # [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
+
+print("\n=== ID 2: Oradea - Eforie ===")
+oe = search.GPSProblem('O', 'E', search.romania)
+
+print("--- Amplitud ---")
+print(search.breadth_first_graph_search(oe).path())
+
+print("\n--- Profundidad ---")
+print(search.depth_first_graph_search(oe).path())
+
+print("\n=== ID 3: Giurgiu - Zerind ===")
+gz = search.GPSProblem('G', 'Z', search.romania)
+
+print("--- Amplitud ---")
+print(search.breadth_first_graph_search(gz).path())
+
+print("\n--- Profundidad ---")
+print(search.depth_first_graph_search(gz).path())
+
+print("\n=== ID 4: Neamt - Dobreta ===")
+nd = search.GPSProblem('N', 'D', search.romania)
+
+print("--- Amplitud ---")
+print(search.breadth_first_graph_search(nd).path())
+
+print("\n--- Profundidad ---")
+print(search.depth_first_graph_search(nd).path())
+
+print("\n=== ID 5: Mehadia - Fagaras ===")
+mf = search.GPSProblem('M', 'F', search.romania)
+
+print("--- Amplitud ---")
+print(search.breadth_first_graph_search(mf).path())
+
+print("\n--- Profundidad ---")
+print(search.depth_first_graph_search(mf).path())
