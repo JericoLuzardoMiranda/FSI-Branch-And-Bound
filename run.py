6dac11fb313bae26329ea5febc597c526870282d
JericoLuzardoMiranda <125827592+JericoLuzardoMiranda@users.noreply.github.com>
2023-10-11 17:15:12 +0100

    Código refactorizado

--- run.py
+++ run.py
@@ -2,51 +2,20 @@
 
 import search
 
-print("=== ID 1: Arad - Bucharest ===")
-ab = search.GPSProblem('A', 'B', search.romania)
 
-print("--- Amplitud ---")
-print(search.breadth_first_graph_search(ab).path())
+def search_route(number, origin, destination, romania):
+    print(f"\n===== ID {number}: {origin} - {destination} =====")
+    gps = search.GPSProblem(origin, destination, romania)
 
-print("\n--- Profundidad ---")
-print(search.depth_first_graph_search(ab).path())
+    print("--- Amplitud ---")
+    print(search.breadth_first_graph_search(gps).path())
 
-# Result:
-# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
-# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
+    print("\n--- Profundidad ---")
+    print(search.depth_first_graph_search(gps).path())
 
-print("\n=== ID 2: Oradea - Eforie ===")
-oe = search.GPSProblem('O', 'E', search.romania)
 
-print("--- Amplitud ---")
-print(search.breadth_first_graph_search(oe).path())
-
-print("\n--- Profundidad ---")
-print(search.depth_first_graph_search(oe).path())
-
-print("\n=== ID 3: Giurgiu - Zerind ===")
-gz = search.GPSProblem('G', 'Z', search.romania)
-
-print("--- Amplitud ---")
-print(search.breadth_first_graph_search(gz).path())
-
-print("\n--- Profundidad ---")
-print(search.depth_first_graph_search(gz).path())
-
-print("\n=== ID 4: Neamt - Dobreta ===")
-nd = search.GPSProblem('N', 'D', search.romania)
-
-print("--- Amplitud ---")
-print(search.breadth_first_graph_search(nd).path())
-
-print("\n--- Profundidad ---")
-print(search.depth_first_graph_search(nd).path())
-
-print("\n=== ID 5: Mehadia - Fagaras ===")
-mf = search.GPSProblem('M', 'F', search.romania)
-
-print("--- Amplitud ---")
-print(search.breadth_first_graph_search(mf).path())
-
-print("\n--- Profundidad ---")
-print(search.depth_first_graph_search(mf).path())
+search_route(1, 'A', 'B', search.romania)
+search_route(2, 'O', 'E', search.romania)
+search_route(3, 'G', 'Z', search.romania)
+search_route(4, 'N', 'D', search.romania)
+search_route(5, 'M', 'F', search.romania)
